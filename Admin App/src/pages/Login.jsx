a4151d6c2db50179093e31670e721c0a17544064
Obar <ducwebdev@gmail.com>
2023-11-03 13:36:18 +0700

    edit login admin page

--- Admin App/src/pages/Login.jsx
+++ Admin App/src/pages/Login.jsx
@@ -1,31 +1,50 @@
 import { Form, redirect, useActionData } from 'react-router-dom'
 import url from '../utils/url'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  margin: auto;
+  max-width: max-content;
+  min-width: 500px;
+  display: flex;
+  justify-content: center;
+  label {
+    display: inline-block;
+    min-width: 100px;
+  }
+  .button button {
+    margin-top: 16px;
+    padding: 8px 16px;
+  }
+`
 
 const Login = () => {
   const data = useActionData()
 
   return (
-    <Form method='post'>
-      <h1>Login</h1>
-      {data?.msg && <div style={{color: 'red'}}>{data.msg}</div>}
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="text" id="email" name='email'/>
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name='password'/>
-      </div>
-      <div>
-        <button>Login</button>
-      </div>
-    </Form>
+    <Container>
+      <Form method='post'>
+        <h1>Login</h1>
+        {data?.msg && <div style={{color: 'red'}}>{data.msg}</div>}
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="text" id="email" name='email'/>
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name='password'/>
+        </div>
+        <div className='button'>
+          <button>Login</button>
+        </div>
+      </Form>
+    </Container>
   )
 }
 
 export default Login
 
-export async function action({ request, params }) {
+export async function action({ request }) {
   try {
     const data = await request.formData()
     const formData = {
